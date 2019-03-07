f02438309ced5c15fbfe30118e606985c2fe79f4
nkfoss <fossx148@morris.umn.edu>
2019-03-07 14:32:44 -0600

    Create ride-list component script and HTML

---
+++ client/src/app/rides/ride-list.component.ts
@@ -0,0 +1,125 @@
+import {Component, OnInit} from '@angular/core';
+import {RideListService} from './ride-list.service';
+import {Ride} from './ride';
+import {Observable} from 'rxjs/Observable';
+import {MatDialog} from '@angular/material';
+import {AddRideComponent} from './add-ride.component';
+
+@Component({
+  selector: 'ride-list-component',
+  templateUrl: 'ride-list.component.html',
+  styleUrls: ['./ride-list.component.css'],
+})
+
+export class RideListComponent implements OnInit {
+  // These are public so that tests can reference them (.spec.ts)
+  public rides: Ride[];
+  public filteredRides: Ride[];
+
+  // These are the target values used in searching.
+  // We should rename them to make that clearer.
+  public rideDestination: string;
+  public rideOrigin: string;
+  public rideDriving: boolean;
+
+  // The ID of the
+  private highlightedID: string = '';
+
+  // Inject the UserListService into this component.
+  constructor(public rideListService: RideListService, public dialog: MatDialog) {
+
+  }
+
+  isHighlighted(ride: Ride): boolean {
+    return ride._id['$oid'] === this.highlightedID;
+  }
+
+  openDialog(): void {
+    const newRide: Ride = {_id: '', destination: '', origin: '', notes: '',
+      driving: null, driver: null, riders: null, roundTrip: null, departureTime: null};
+    const dialogRef = this.dialog.open(AddRideComponent, {
+      width: '500px',
+      data: {ride: newRide}
+    });
+
+    dialogRef.afterClosed().subscribe(newRide => {
+      if (newRide != null) {
+        this.rideListService.addNewRide(newRide).subscribe(
+          result => {
+            this.highlightedID = result;
+            this.refreshRides();
+          },
+          err => {
+            // This should probably be turned into some sort of meaningful response.
+            console.log('There was an error adding the ride.');
+            console.log('The newRide or dialogResult was ' + newRide);
+            console.log('The error was ' + JSON.stringify(err));
+          });
+      }
+    });
+  }
+
+  public filterRides(searchDestination: string, searchOrigin: string): Ride[] {
+
+    this.filteredRides = this.rides;
+
+    // Filter by destination
+    if (searchDestination != null) {
+      searchDestination = searchDestination.toLocaleLowerCase();
+
+      this.filteredRides = this.filteredRides.filter(ride => {
+        return !searchDestination || ride.destination.toLowerCase().indexOf(searchDestination) !== -1;
+      });
+    }
+
+    // Filter by origin
+    if (searchOrigin != null) {
+      searchOrigin = searchOrigin.toLocaleLowerCase();
+
+      this.filteredRides = this.filteredRides.filter(ride => {
+        return !searchOrigin || ride.destination.toLowerCase().indexOf(searchOrigin) !== -1;
+      });
+    }
+  }
+
+  /**
+   * Starts an asynchronous operation to update the users list
+   *
+   */
+  refreshRides(): Observable<Ride[]> {
+    // Get Rides returns an Observable, basically a "promise" that
+    // we will get the data from the server.
+    //
+    // Subscribe waits until the data is fully downloaded, then
+    // performs an action on it (the first lambda)
+
+    const rides: Observable<Ride[]> = this.rideListService.getRides();
+    rides.subscribe(
+      rides => {
+        this.rides = rides;
+        this.filterRides(this.rideDestination, this.rideOrigin);
+      },
+      err => {
+        console.log(err);
+      });
+    return rides;
+  }
+
+  ///FIX THIS ///
+  loadService(): void {
+    this.rideListService.getRides(this.rideDriving).subscribe(
+      rides => {
+        this.rides = rides;
+        this.filteredRides = this.rides;
+      },
+      err => {
+        console.log(err);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.refreshRides();
+    this.loadService();
+  }
+}
