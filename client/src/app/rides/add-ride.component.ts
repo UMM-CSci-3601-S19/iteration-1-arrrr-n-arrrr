77c8c78fcf85e4e4720a14f6bcfb5baa95dc185c
nkfoss <nathankfoss@gmail.com>
2019-03-07 22:31:21 -0600

    Add form validators for ride

--- client/src/app/rides/add-ride.component.ts
+++ client/src/app/rides/add-ride.component.ts
@@ -3,6 +3,7 @@ import {MAT_DIALOG_DATA} from '@angular/material';
 import {Ride} from './ride';
 import {FormControl, Validators, FormGroup, FormBuilder} from "@angular/forms";
 
+
 @Component({
   selector: 'add-ride.component',
   templateUrl: 'add-ride.component.html',
@@ -18,15 +19,9 @@ export class AddRideComponent implements OnInit {
   // not sure if this name is magical and making it be found or if I'm missing something,
   // but this is where the red text that shows up (when there is invalid input) comes from
   add_user_validation_messages = {
-    'title': [
-      {type: 'required', message: 'Title is required'},
-      {type: 'minlength', message: 'Title must be at least 5 characters long'},
-      {type: 'maxlength', message: 'Title cannot be more than 100 characters long'},
-      {type: 'pattern', message: 'Title must contain only numbers, letters, punctuation or paranthesis'}
-    ],
 
-    'body': [
-      {type: 'required', message: 'Body is required'}
+    'notes': [
+      {type: 'required', message: 'Notes are required'}
     ],
 
     'destination' : [
@@ -34,32 +29,46 @@ export class AddRideComponent implements OnInit {
       {type: 'minlength', message: 'Destination must be at least 3 characters long'},
       {type: 'maxlength', message: 'Destination cannot be more than 30 characters long'},
       {type: 'pattern', message: 'Destination must contain only numbers, letters, punctuation, or paranthesis'}
+    ],
+
+    'origin' : [
+      {type: 'required', message: 'Origin is required'},
+      {type: 'minlength', message: 'Origin must be at least 3 characters long'},
+      {type: 'maxlength', message: 'Origin cannot be more than 30 characters long'},
+      {type: 'pattern', message: 'Origin must contain only numbers, letters, punctuation, or paranthesis'}
+    ],
+
+    'driving' : [
+      {type: 'required', message: 'You must indicate whether you are the driver or not'}
     ]
+
   };
 
   createForms() {
 
     // add ride form validations
     this.addRideForm = this.fb.group({
-      // We allow alphanumeric input, with the option of punctuation, and limit the length for title.
-      title: new FormControl('title', Validators.compose([
-        Validators.minLength(5),
-        Validators.maxLength(100),
-        Validators.pattern('[a-zA-Z0-9.?!(),\'\"]'),
+      // Notes is only required, with few restrictions.
+      notes: new FormControl('notes', Validators.compose([
         Validators.required
       ])),
 
-      // Body is only required, with few restrictions.
-      body: new FormControl('body', Validators.compose([
-        Validators.required
-      ])),
-
-      // Body is only required, with few restrictions.
       destination: new FormControl('destination', Validators.compose([
         Validators.required,
         Validators.minLength(3),
         Validators.maxLength(30),
         Validators.pattern('[a-zA-Z0-9.?!(),\'\"]'),
+      ])),
+
+      origin: new FormControl('origin', Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(30),
+        Validators.pattern('[a-zA-Z0-9.?!(),\'\"]'),
+      ])),
+
+      driving: new FormControl('driving', Validators.compose([
+        Validators.required
       ])),
 
     })
